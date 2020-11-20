b6981fe05656819bf4c7f62380fc9a3c6dd1fb9b
Toluwalase <tolu@cottacush.com>
2020-11-20 08:07:39 +0100

    updated the courses route

--- routes/courses.js
+++ routes/courses.js
@@ -6,22 +6,35 @@ const router = express.Router();
 // Handler function to wrap each route.
 const { asyncHandler } = require('../middleware/async-handler');
 const { authenticateUser } = require('../middleware/auth-user');
-const { Course } = require('../models/course');
-
-const courses = [];
+const { Course } = require('../models');
 
 // Returns a list of courses (including the user that owns each course)
 router.get('/courses', asyncHandler(async (req, res) => {
-    await res.json(courses);
-    res.status(200).end();
+    const courses = await Course.findAll({
+        attributes: {
+            exclude: ["createdAt", "updatedAt"]
+        }
+    });
+    res.status(200).json(courses);
 }));
 
 // Returns the course (including the user that owns the course) for the provided course ID
 router.get('/courses/:id', asyncHandler(async (req, res) => {
+    let course;
     try {
-        const course = await Course.findByPk(req.params.id);
-        res.json(course);
-        res.status(200).end();
+        course = await Course.findByPk(req.params.id, {
+            attributes: {
+                exclude: ["createdAt", "updatedAt"]
+            }
+        });
+
+        if (course) {
+            res.status(200).json(course);
+        } else {
+            const error = new Error('The course you are looking for does not exist.');
+            error.status = 404;
+            throw error;
+        }
     } catch (error) {
         if (error === 'SequelizeValidationError' || error === 'SequelizeUniqueConstraintError') {
             const errors = error.errors.map(err => err.message);
@@ -38,8 +51,8 @@ router.post('/courses', authenticateUser, asyncHandler(async(req, res) => {
         // Get the course from the request body.
         const course = await Course.create(req.body);
         // Get New course
-        const courseData = course.get({ plain:true });
-        res.status(201).location(`/course/${ courseData.id }`).end();
+        const courseData = await Course.findOne({ where: { title: req.body.title } });
+        res.status(201).location(`/api/courses/${ courseData.id }`).end();
     } catch (error) {
         if (error.name === 'SequelizeValidationError' || error.name === 'SequelizeUniqueConstraintError') {
             const errors = error.errors.map(err => err.message);
@@ -48,20 +61,49 @@ router.post('/courses', authenticateUser, asyncHandler(async(req, res) => {
             throw error;
         }
     }
-
 }));
 
 // Updates a course and returns no content
 router.put('/courses/:id', authenticateUser, asyncHandler(async (req, res) => {
-    const course = await Course.findByPk(req.params.id);
-    await course.update(req.body);
-    res.status(204).end();
+    let course;
+    try {
+        course = await Course.findByPk(req.params.id);
+        if (course) {
+            if (course.userId === req.currentUser.id) {
+                await course.update(req.body);
+                res.status(204).end();
+            } else {
+                res.status(403)
+            }
+        } else {
+            const error = new Error('The course you are looking for does not exist.');
+            error.status = 404;
+            throw error;
+        }
+    } catch (error) {
+        if (error.name === 'SequelizeValidationError' || error.name === 'SequelizeUniqueConstraintError') {
+            const errors = error.errors.map(err => err.message);
+            res.status(400).json({ errors });
+        } else {
+            throw error;
+        }
+    }
 }));
 
 router.delete('/courses/:id', authenticateUser, asyncHandler(async (req, res) => {
     const course = await Course.findByPk(req.params.id);
-    await course.destroy();
-    res.status(204).end();
+    if (course) {
+        if (course.userId === req.currentUser.id) {
+            await course.destroy();
+            res.status(204).end();
+        } else {
+            res.status(403)
+        }
+    } else {
+        const error = new Error('The course you are looking for does not exist.');
+        error.status = 404;
+        throw error;
+    }
 }));
 
 module.exports = router;
